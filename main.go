036d51e01d0c238ed54fc183d349df13be15d406
Gergely Tarkó <hergo97@gmail.com>
2022-10-24 12:00:43 +0200

    Revert main.go

--- main.go
+++ main.go
@@ -2,61 +2,59 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"path/filepath"
 	"vizsla/utils"
 )
 
 func main() {
 
-	// if len(os.Args) < 2 {
-	// 	return
-	// }
+	if len(os.Args) < 2 {
+		return
+	}
 
-	// for _, arg := range os.Args[1:] {
-	// 	if t, err := utils.Get(arg); err == nil {
-	// 		fmt.Printf("%s:\t%s\n", arg, t)
-	// 	} else {
-	// 		fmt.Printf("%s: Unknown\n", arg)
+	for _, arg := range os.Args[1:] {
+		if t, err := utils.Get(arg); err == nil {
+			fmt.Printf("%s:\t%s\n", arg, t)
+		} else {
+			fmt.Printf("%s: Unknown\n", arg)
+		}
+	}
+
+	// filepath.Walk("tests", func(path string, info os.FileInfo, err error) error {
+	// 	if err != nil {
+	// 		log.Fatalf(err.Error())
 	// 	}
-	// }
+	// 	//file, _ := os.OpenFile(path, os.O_RDONLY, 0666)
+	// 	//defer file.Close()
+	// 	//var contentByte = make([]byte, 2048)
+	// 	//numByte, _ := file.Read(contentByte)
+	// 	//contentByte = contentByte[:numByte]
 
-	filepath.Walk("tests", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-		//file, _ := os.OpenFile(path, os.O_RDONLY, 0666)
-		//defer file.Close()
-		//var contentByte = make([]byte, 2048)
-		//numByte, _ := file.Read(contentByte)
-		//contentByte = contentByte[:numByte]
+	// 	//contentByte, err := ioutil.ReadFile(path)
 
-		//contentByte, err := ioutil.ReadFile(path)
+	// 	//if strings.HasSuffix(path, ".vdi") || strings.HasSuffix(path, ".doc") || strings.HasSuffix(path, ".ppt") {
+	// 	if t, err := utils.Get(path); err == nil {
 
-		//if strings.HasSuffix(path, ".vdi") || strings.HasSuffix(path, ".doc") || strings.HasSuffix(path, ".ppt") {
-		if t, err := utils.Get(path); err == nil {
+	// 		fmt.Printf("File Name: %s\t\t%s\n", info.Name(), t)
 
-			fmt.Printf("File Name: %s\t\t%s\n", info.Name(), t)
+	// 	}
+	// 	//}
+	// 	// 	// check := []byte{
+	// 	// 	// 	0x4d,
+	// 	// 	// 	0x45,
+	// 	// 	// 	0x54,
+	// 	// 	// 	0x41,
+	// 	// 	// 	0x2d,
+	// 	// 	// 	0x49,
+	// 	// 	// 	0x4e,
+	// 	// 	// 	0x46,
+	// 	// 	// 	0x2f}
+	// 	// 	// if compare(contentByte[30:39], check) {
+	// 	// 	// 	fmt.Println(path)
+	// 	// 	// }
 
-		}
-		//}
-		// 	// check := []byte{
-		// 	// 	0x4d,
-		// 	// 	0x45,
-		// 	// 	0x54,
-		// 	// 	0x41,
-		// 	// 	0x2d,
-		// 	// 	0x49,
-		// 	// 	0x4e,
-		// 	// 	0x46,
-		// 	// 	0x2f}
-		// 	// if compare(contentByte[30:39], check) {
-		// 	// 	fmt.Println(path)
-		// 	// }
-
-		return nil
-	})
+	// 	return nil
+	// })
 }
 
 // func compare(a []byte, b []byte) bool {
